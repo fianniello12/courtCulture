f7ab5db5725291899b0a90cfb1bb7a93e222b44a
fianniello12 <f.ianniello12@studenti.unisa.it>
2026-05-27 22:52:17 +0200

    create prodottoDao and the package

---
+++ src/main/java/it/unisa/courtCulture/dao/ProdottoDao.java
@@ -0,0 +1,20 @@
+package it.unisa.courtCulture.dao;
+
+import java.sql.SQLException;
+import java.util.Collection;
+
+import it.unisa.courtCulture.model.ProdottoBean;
+
+
+public interface ProdottoDao {
+	
+	public void doSave(ProdottoBean prodotto) throws SQLException;
+	
+	public boolean doUpdateImage(ProdottoBean prodotto) throws SQLException;
+
+	public boolean doDelete(int code) throws SQLException;
+
+	public ProdottoBean doRetrieveByKey(int code) throws SQLException;
+	
+	public Collection<ProdottoBean> doRetrieveAll(String order) throws SQLException;
+}
